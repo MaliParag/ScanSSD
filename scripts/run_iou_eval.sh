01830afc6dec6efeeb63b61f887f66983dd9e666
paragmali <parag201093@gmail.com>
2019-04-23 22:36:00 -0400

    Adding grid search script

---
+++ scripts/run_iou_eval.sh
@@ -0,0 +1,11 @@
+for i in {0..5..5}
+do
+        python3 /home/psm2208/Workspace/IOU_lib/IOUevaluater.py --detections /home/psm2208/code/eval/Train3_Focal_10_25/voting_equal_$i.0/ --ground_truth /home/psm2208/Workspace/Task3_Detection/Train/GT_math/GT_math/ > voting_equal_$i.result &
+        python3 /home/psm2208/Workspace/IOU_lib/IOUevaluater.py --detections /home/psm2208/code/eval/Train3_Focal_10_25/voting_equal_$((i+1)).0/ --ground_truth /home/psm2208/Workspace/Task3_Detection/Train/GT_math/GT_math/ > voting_equal_$((i+1)).result &
+        python3 /home/psm2208/Workspace/IOU_lib/IOUevaluater.py --detections /home/psm2208/code/eval/Train3_Focal_10_25/voting_equal_$((i+2)).0/ --ground_truth /home/psm2208/Workspace/Task3_Detection/Train/GT_math/GT_math/ > voting_equal_$((i+2)).result &
+        python3 /home/psm2208/Workspace/IOU_lib/IOUevaluater.py --detections /home/psm2208/code/eval/Train3_Focal_10_25/voting_equal_$((i+3)).0/ --ground_truth /home/psm2208/Workspace/Task3_Detection/Train/GT_math/GT_math/ > voting_equal_$((i+3)).result &
+        python3 /home/psm2208/Workspace/IOU_lib/IOUevaluater.py --detections /home/psm2208/code/eval/Train3_Focal_10_25/voting_equal_$((i+4)).0/ --ground_truth /home/psm2208/Workspace/Task3_Detection/Train/GT_math/GT_math/ > voting_equal_$((i+4)).result &
+
+        wait
+
+done
